078e4f1e1acc673513325ecaf98187ef8fe222ae
petkapiter <Petr.Merkulov@gmail.com>
2015-03-20 16:13:47 +0300

    end day

--- cisco_errdisable.pl
+++ cisco_errdisable.pl
@@ -7,13 +7,18 @@
 	chomp (my $login = <STDIN>);
 	print "Password: ";
 	chomp (my $password = <STDIN>);
+	print "Enable Password: ";
+        chomp (my $secret = <STDIN>);
 	my $ssh = Net::Appliance::Session->new({ 
 		personality => 'ios',
 		transport => 'SSH',
 		host => $host});
 	$ssh->connect({
 		username => $login, 
-		password => $password
+		password => $password,
+		privileged_password => $secret
 		});
-	print $ssh->cmd('show errdisable recovery');
+	$ssh->begin_privileged;
+	print $ssh->cmd('show logging');
+	$ssh->end_privileged;
 	$ssh ->close;
