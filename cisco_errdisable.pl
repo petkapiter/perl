e3167107e70693e6401f3ee08387061ee887977c
petkapiter <Petr.Merkulov@gmail.com>
2015-03-20 23:22:50 +0300

    Use Getopt and stdin

--- cisco_errdisable.pl
+++ cisco_errdisable.pl
@@ -4,21 +4,32 @@
 	use Net::Appliance::Session;
 	use Getopt::Long;
 	
-	my $host = '';
-	my $login = '';
-	my $password = '';
-	my $secret = '';
+	my $host;
+	my $login;
+	my $password;
+	my $secret;
 	GetOptions('host=s' =>\$host, 'login=s' => \$login, 'password=s' => \$password, 'enable=s' => \$secret);
 
-#	print "Host IP: " ;
-#	chomp (my $host = <STDIN>);
-#	print "Username: ";
-#	chomp (my $login = <STDIN>);
-#	print "Password: ";
-#	chomp (my $password = <STDIN>);
-#	print "Enable Password: ";
-#        chomp (my $secret = <STDIN>);
-	
+		unless (defined $host){
+			print "Host IP: " ;
+			chomp ($host = <STDIN>);
+		}
+
+		unless (defined $login){
+			print "Username: ";
+			chomp ($login = <STDIN>);
+		}
+
+			unless (defined $password){
+			print "Password: ";
+			chomp ($password = <STDIN>);
+		}
+
+		unless (defined $secret){
+			print "Enable Password: ";
+		        chomp ($secret = <STDIN>);
+		}
+
 	my $ssh = Net::Appliance::Session->new({ 
 		personality => 'ios',
 		transport => 'SSH',
