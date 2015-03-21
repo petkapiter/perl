32b74dce20cd4208737bdd216e4fbf5ecf66db34
petkapiter <Petr.Merkulov@gmail.com>
2015-03-21 15:33:11 +0300

    versin 0.2 all rewrite

--- cisco_errdisable.pl
+++ cisco_errdisable.pl
@@ -4,55 +4,73 @@
 	use Net::Appliance::Session;
 	use Getopt::Long;
 	
-	my $help;
-	my $host;
-	my $login;
-	my $password;
-	my $secret;
-	GetOptions('help' =>\$help, 'ip=s' =>\$host, 'login=s' => \$login, 'password=s' => \$password, 'enable=s' => \$secret);
+	my $version = "0.2";
+	my %opt = (
+			"version"	=> 0,
+			"help"		=> 0,
+			"ip"		=> 0,
+			"login"		=> 0,
+			"password"	=> 0,
+			"secret"	=> 0,
+	);
 
-		unless (defined $help){
-		print "-i, --ip		Host IP\n";
-		print "-l, --login		Username\n";
-		print "-p, --password		Password\n";
-		print "-e, --enable		Secret Password\n";
-		print "-h, --help		display this help and exit\n";
-		exit;
-		}
+	GetOptions(\%opt,
+			'version',
+			'help',
+			'ip=s',
+			'login=s',
+			'password=s',
+			'secret=s',
+	);
+	if (defined $opt{'help'} && $opt{'help'} == 1) { 
+			&usage;
+	}
 
-		unless (defined $host){
+	sub usage {
+			 print "-i, --ip		Host IP\n".
+			 "-l, --login		Username\n".
+			 "-p, --password		Password\n".
+			 "-e, --enable		Secret Password\n".
+			 "-h, --help		Display this help and exit\n".
+			 "-v, --version		Print version info\n";
+			 exit;
+	}
+
+	if (defined $opt{'version'} && $opt{'version'} == 1) {
+			print "Cisco_errdisable - (C) 2015 version $version\n";
+			exit;
+	}
+
+	if (defined $opt{'ip'} && $opt{'ip'} eq 0){
 			print "Host IP: " ;
-			chomp ($host = <STDIN>);
-		}
+			chomp ($opt{'ip'} = <STDIN>);
+	}
+	 if (defined $opt{'login'} && $opt{'login'} eq 0){
+	 		print "Username: " ;
+	 		chomp ($opt{'login'} = <STDIN>);
+	}
 
-		unless (defined $login){
-			print "Username: ";
-			chomp ($login = <STDIN>);
-		}
-
-			unless (defined $password){
+	if (defined $opt{'password'} && $opt{'password'} eq 0){
 			print "Password: ";
-			chomp ($password = <STDIN>);
-		}
-
-		unless (defined $secret){
+			chomp ($opt{'password'} = <STDIN>);
+	}
+	if (defined $opt{'secret'} && $opt{'secret'} eq 0){
 			print "Enable Password: ";
-		        chomp ($secret = <STDIN>);
-		}
+			chomp ($opt{'secret'} = <STDIN>);
+	}
+
 
 	my $ssh = Net::Appliance::Session->new({ 
 		personality => 'ios',
 		transport => 'SSH',
-		host => $host});
+		host => $opt{ip}
+	});
 	$ssh->connect({
-		username => $login, 
-		password => $password,
-		privileged_password => $secret
+		username => $opt{login}, 
+		password => $opt{password},
+		privileged_password => $opt{secret}
 		});
 	$ssh->begin_privileged;
 	print $ssh->cmd('show errdisable recovery');
 	$ssh->end_privileged;
 	$ssh ->close;
-
-	
-	
