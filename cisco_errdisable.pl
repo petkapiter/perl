b26e57f42af37b4fb27e4f8528330cc5620e9141
petkapiter <Petr.Merkulov@gmail.com>
2015-03-25 11:03:57 +0300

    question work

--- cisco_errdisable.pl
+++ cisco_errdisable.pl
@@ -1,96 +1,97 @@
 #!/usr/bin/perl
-	use strict;
-	use warnings;
-	use Net::Appliance::Session; #Cisco ssh
-	use Getopt::Long; #add help
-	
-	my $version = "0.3.1";
-	my $port;
-	my $mac;
-	my %opt = (
-			"version"	=> 0,
-			"help"		=> 0,
-			"ip"		=> 0,
-			"login"		=> 0,
-			"password"	=> 0,
-			"enable"	=> 0,
-	);
-	
-	GetOptions(\%opt,
-			'version',
-			'help',
-			'ip=s',
-			'login=s',
-			'password=s',
-			'enable=s',
-	);
-	if (defined $opt{'help'} && $opt{'help'} == 1) { 
-			&usage;
-	}
 
+use strict;
+use warnings;
+use Net::Appliance::Session; #Cisco ssh
+use Getopt::Long; #add help
+	
+my $version = "0.3.2";
+my $port;
+my $mac;
+our $question;
+my %opt = (
+		"version"	=> 0,
+		"help"		=> 0,
+		"ip"		=> 0,
+		"login"		=> 0,
+		"password"	=> 0,
+		"enable"	=> 0,
+);
+
+GetOptions(\%opt,
+		'version',
+		'help',
+		'ip=s',
+		'login=s',
+		'password=s',
+		'enable=s',
+);
+if (defined $opt{'help'} && $opt{'help'} == 1) { 
+		&usage;
+}
 	sub usage {
-			 print 
-			 "-i, --ip		Host IP\n".
-			 "-l, --login		Username\n".
-			 "-p, --password		Password\n".
-			 "-e, --enable		Secret Password\n".
-			 "-h, --help		Display this help and exit\n".
-			 "-v, --version		Print version info\n";
-			 exit;
-	}
-
+		 print 
+		 "-i, --ip		Host IP\n".
+		 "-l, --login		Username\n".
+		 "-p, --password		Password\n".
+		 "-e, --enable		Secret Password\n".
+		 "-h, --help		Display this help and exit\n".
+		 "-v, --version		Print version info\n";
+		 exit;
+}
 	if (defined $opt{'version'} && $opt{'version'} == 1) {
-			print "Cisco_errdisable - (C) 2015 version $version\n";
-			exit;
-	}
-
-	#if without parametrs
-	if (defined $opt{'ip'} && $opt{'ip'} eq 0){
-			print "Host IP: " ;
-			chomp ($opt{'ip'} = <STDIN>);
-	}
-	 if (defined $opt{'login'} && $opt{'login'} eq 0){
-	 		print "Username: " ;
-	 		chomp ($opt{'login'} = <STDIN>);
-	}
-
-	if (defined $opt{'password'} && $opt{'password'} eq 0){
-			print "Password: ";
-			chomp ($opt{'password'} = <STDIN>);
-	}
-	if (defined $opt{'enable'} && $opt{'enable'} eq 0){
-			print "Enable Password: ";
-			chomp ($opt{'enable'} = <STDIN>);
-	}
-		
-	my $ssh = Net::Appliance::Session->new({ 
-		personality => 'ios',
-		transport => 'SSH',
-		timeout => '4',
-		host => $opt{ip}
-	});
-	$ssh->connect({
-		username => $opt{login}, 
-		password => $opt{password}
-		}) ;
-	$ssh->begin_privileged({ password => $opt{enable}});
-	my $errdisable = $ssh->cmd( 'show errdisable recovery' );
-	if ($errdisable =~ m/Gi\w+\/\w+\/\w+/i){ #Вычленяем строку вида gi91/0/1
-		$port = $&;
-		print "Found errdisable port $port.\n";
-	}else {
-		print "Not found errdisable port.\n";
-		$ssh->end_privileged;
-		$ssh ->close;
+		print ( "Cisco_errdisable - (C) 2015 version $version\n" );
 		exit;
-	}
+}
+	#if without parametrs
+if (defined $opt{'ip'} && $opt{'ip'} eq 0){
+		print ( "Host IP: " );
+		chomp ($opt{'ip'} = <STDIN>);
+}
+ if (defined $opt{'login'} && $opt{'login'} eq 0){
+ 		print ( "Username: " );
+ 		chomp ($opt{'login'} = <STDIN>);
+}
+	if (defined $opt{'password'} && $opt{'password'} eq 0){
+		print ( "Password: " );
+		chomp ($opt{'password'} = <STDIN>);
+}
+if (defined $opt{'enable'} && $opt{'enable'} eq 0){
+		print ( "Enable Password: " );
+		chomp ($opt{'enable'} = <STDIN>);
+}
 	
+my $ssh = Net::Appliance::Session->new({ 
+	personality => 'ios',
+	transport => 'SSH',
+	timeout => '4',
+	host => $opt{ip}
+});
+$ssh->connect({
+	username => $opt{login}, 
+	password => $opt{password}
+	}) ;
+my $errdisable = $ssh->cmd( 'show errdisable recovery' );
+print ( "$errdisable\n" );
+
+if ($errdisable =~ m/Gi\w+\/\w+\/\w+/i){ #Вычленяем строку вида gi91/0/1
+	$port = $&;
+	$question = yanq();
+	print ( "$question\n" );
+
+}else {
+	print ( "Not found errdisable port.\n" );
+	$ssh ->close; 
+	exit;
+}
+if ($question =~ /y/i){
+	$ssh->begin_privileged({ password => $opt{enable}});
 	my $port_sec = $ssh->cmd( "show port-security interface $port | include Last" ); 
 		if ($port_sec =~ m/(\w+.\w+.\w+):\d/i){
 			$mac = $1;
-			print "Last mac adress $mac.\n";
+			print ( "Bad mac adress $mac.\n" );
 		}else{
-			print "Not found mac address.\n";
+			print ( "Not found mac address.\n" );
 			$ssh->end_privileged;
 			$ssh ->close;
 			exit;
@@ -105,18 +106,20 @@
 	$ssh->end_configure;
 	$ssh->end_privileged;
 	$ssh ->close;
-	print "Interface - $port cleared.\n";
-	print "MAC address - $mac cleared\n";
-
-	sub yan {
-			use Term::ReadKey;
-			ReadMode 4;
-			my $key = '';
-			while ($key !~ /(Y|N)/i){
-				1 while defined ReadKey -1; # discard any previous input
-				print "Type Y/N: ";
-				$key = ReadKey 0; # read a single character
-			}
-			ReadMode 0;
-			return $key
+	print ( "Interface - $port cleared.\n" );
+	print ( "MAC address - $mac cleared\n" );
+}else {
+	print ( "Cancel\n" );
+}
+sub yanq {
+	use Term::ReadKey;
+	ReadMode 4;
+	my $key = '';
+	while ($key !~ /(Y|N)/i){
+		1 while defined ReadKey -1; # discard any previous input
+		print ( "Do you wont clear $port (Y/N): " );
+		$key = ReadKey 0; # read a single character
+	}
+	ReadMode 0;
+	return $key
 }
